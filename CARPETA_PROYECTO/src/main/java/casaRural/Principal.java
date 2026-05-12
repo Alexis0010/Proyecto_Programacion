daeac6f1c5f9c15ad745383a90aeabbc99237f02
aenol <ojy15330@educastur.es>
2026-05-12 16:45:15 +0200

    seguimos

--- CARPETA_PROYECTO/src/main/java/casaRural/Principal.java
+++ CARPETA_PROYECTO/src/main/java/casaRural/Principal.java
@@ -1,18 +1,30 @@
 package casaRural;
 
+import java.io.Writer;
 import java.util.Scanner;
 
 public class Principal {
     public static Scanner t=new Scanner(System.in);
+    Habitacion habitacion;
 
     public void run(){
 
         eliminarHabitacion();
+        crearFactura();
     }
 
     public void eliminarHabitacion(){
         System.out.println("Que habitacion quieres eliminar: ");
         int habitacionAEliminar=t.nextInt();
+        if (habitacionAEliminar==habitacion.id){
+
+        }
+
+    }
+
+    public void crearFactura(){
+        System.out.println("Creando factura....");
+
 
     }
 
